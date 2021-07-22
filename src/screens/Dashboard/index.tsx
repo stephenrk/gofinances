33671c4d32322d75b0e201cc10c39859b9937bfa
Stephen Rodrigues Knupfer <srodrigues@springglobal.com>
2021-07-21 22:58:54 -0300

    Criacao do projeto; Estilizando componentes com styled-components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { HighlightCard } from '../../components/HighlightCard'
+import { TransactionCard } from '../../components/TransactionCard'
+
+import {
+    Container, Header, Photo, User, UserGreeting, UserInfo, UserName, UserWrapper, Icon, HighlightCards, Transactions, Title } from './styles'
+
+export const Dashboard = () => {
+    return (
+        <Container>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/15800445?v=4' }} />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Stephen</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
+
+            <HighlightCards>
+                <HighlightCard type="up" title="Entradas" amount="R$ 17.400,00" lastTransaction="Última entrada dia 13 de abril" />
+                <HighlightCard type="down" title="Saídas" amount="R$ 1.259,00" lastTransaction="Última saída dia 03 de abril" />
+                <HighlightCard type="total" title="Total" amount="R$ 16.141,00" lastTransaction="01 à 16 de abril" />
+            </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+                <TransactionCard />
+            </Transactions>
+        </Container>
+    )
+}
